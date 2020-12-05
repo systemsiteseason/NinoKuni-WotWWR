1b6e6eae94b3cb819eb232687fbec4fe5a172f45
Son Hoang <haitac865@gmail.com>
2020-12-05 20:29:38 +0700

    support get and repack texture

--- NinoKuni/main.Designer.cs
+++ NinoKuni/main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NinoKuni
 {
-    partial class Form1
+    partial class main
     {
         /// <summary>
         /// Required designer variable.
@@ -28,11 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.getTableCharacterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.getTextureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.defaultToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -50,7 +51,7 @@
             this.aboutMeToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1031, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1034, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -63,6 +64,20 @@
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
             // toolsToolStripMenuItem
             // 
             this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -72,24 +87,10 @@
             this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
             this.toolsToolStripMenuItem.Text = "Tools";
             // 
-            // openToolStripMenuItem
-            // 
-            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.openToolStripMenuItem.Text = "Open";
-            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
-            // 
             // getTableCharacterToolStripMenuItem
             // 
             this.getTableCharacterToolStripMenuItem.Name = "getTableCharacterToolStripMenuItem";
-            this.getTableCharacterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.getTableCharacterToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
             this.getTableCharacterToolStripMenuItem.Text = "Get Table Character";
             // 
             // getTextureToolStripMenuItem
@@ -98,19 +99,19 @@
             this.defaultToolStripMenuItem,
             this.noAlphaToolStripMenuItem});
             this.getTextureToolStripMenuItem.Name = "getTextureToolStripMenuItem";
-            this.getTextureToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.getTextureToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
             this.getTextureToolStripMenuItem.Text = "Get Texture...";
             // 
             // defaultToolStripMenuItem
             // 
             this.defaultToolStripMenuItem.Name = "defaultToolStripMenuItem";
-            this.defaultToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.defaultToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
             this.defaultToolStripMenuItem.Text = "Default";
             // 
             // noAlphaToolStripMenuItem
             // 
             this.noAlphaToolStripMenuItem.Name = "noAlphaToolStripMenuItem";
-            this.noAlphaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.noAlphaToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
             this.noAlphaToolStripMenuItem.Text = "No Alpha";
             // 
             // aboutMeToolStripMenuItem
@@ -119,15 +120,17 @@
             this.aboutMeToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
             this.aboutMeToolStripMenuItem.Text = "AboutMe";
             // 
-            // Form1
+            // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1031, 581);
+            this.ClientSize = new System.Drawing.Size(1034, 581);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ni no Kuni Wrath of the White Witch Remastered Texture Tool";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
--- NinoKuni/main.cs
+++ NinoKuni/main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,12 @@ using System.Windows.Forms;
 
 namespace NinoKuni
 {
-    public partial class Form1 : Form
+    public partial class main : Form
     {
-        public Form1()
+        public main()
         {
             InitializeComponent();
+            this.MinimumSize = new Size(600, 400);
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -24,12 +26,23 @@ namespace NinoKuni
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            opf.Filter = "All file(*.*)|*.cfg.bin;*.p3igg;*.p3img|All Font or Table Chars|*.cfg.bin|Header Texture|*.p3img|Texture|*.p3igg";
+            opf.Filter = "All file(*.*)|*.cfg.bin;*.p3igg;*.p3img;*.imgpak|All Font or Table Chars|*.cfg.bin|Header Texture|*.p3img|Texture|*.p3igg|Packet Texture|*.imgpak";
             if(opf.ShowDialog() == DialogResult.OK)
             {
                 if (!string.IsNullOrEmpty(opf.FileName))
                 {
+                    if(Path.GetExtension(opf.FileName) == ".cfg.bin")
+                    {
 
+                    }
+                    else if(Path.GetExtension(opf.FileName) == ".p3img")
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
                 }
             }
         }

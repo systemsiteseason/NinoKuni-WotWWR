d87be6fbdf0593cf9b95c4652bda5ce678bec934
Son Hoang <haitac865@gmail.com>
2020-12-10 19:42:40 +0700

    New Update

--- NinoKuni/main.Designer.cs
+++ NinoKuni/main.Designer.cs
@@ -35,12 +35,26 @@
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.getTableCharacterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.getTextureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.defaultToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.noAlphaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutMeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.opf = new System.Windows.Forms.OpenFileDialog();
+            this.panelBox = new System.Windows.Forms.FlowLayoutPanel();
+            this.boxTexture = new System.Windows.Forms.PictureBox();
+            this.gr1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gr2 = new System.Windows.Forms.GroupBox();
+            this.zoomtrack = new System.Windows.Forms.TrackBar();
+            this.fName = new System.Windows.Forms.Label();
+            this.len = new System.Windows.Forms.Label();
+            this.offsetF = new System.Windows.Forms.Label();
+            this.typeF = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
+            this.panelBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.boxTexture)).BeginInit();
+            this.gr1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.zoomtrack)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -67,22 +81,22 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // toolsToolStripMenuItem
             // 
             this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.getTableCharacterToolStripMenuItem,
-            this.getTextureToolStripMenuItem});
+            this.getTableCharacterToolStripMenuItem});
+            this.toolsToolStripMenuItem.Enabled = false;
             this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
             this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
             this.toolsToolStripMenuItem.Text = "Tools";
@@ -90,29 +104,8 @@
             // getTableCharacterToolStripMenuItem
             // 
             this.getTableCharacterToolStripMenuItem.Name = "getTableCharacterToolStripMenuItem";
-            this.getTableCharacterToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
-            this.getTableCharacterToolStripMenuItem.Text = "Get Table Character";
-            // 
-            // getTextureToolStripMenuItem
-            // 
-            this.getTextureToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.defaultToolStripMenuItem,
-            this.noAlphaToolStripMenuItem});
-            this.getTextureToolStripMenuItem.Name = "getTextureToolStripMenuItem";
-            this.getTextureToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
-            this.getTextureToolStripMenuItem.Text = "Get Texture...";
-            // 
-            // defaultToolStripMenuItem
-            // 
-            this.defaultToolStripMenuItem.Name = "defaultToolStripMenuItem";
-            this.defaultToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
-            this.defaultToolStripMenuItem.Text = "Default";
-            // 
-            // noAlphaToolStripMenuItem
-            // 
-            this.noAlphaToolStripMenuItem.Name = "noAlphaToolStripMenuItem";
-            this.noAlphaToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
-            this.noAlphaToolStripMenuItem.Text = "No Alpha";
+            this.getTableCharacterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.getTableCharacterToolStripMenuItem.Text = "Save new file";
             // 
             // aboutMeToolStripMenuItem
             // 
@@ -120,11 +113,143 @@
             this.aboutMeToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
             this.aboutMeToolStripMenuItem.Text = "AboutMe";
             // 
+            // panelBox
+            // 
+            this.panelBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelBox.AutoScroll = true;
+            this.panelBox.Controls.Add(this.boxTexture);
+            this.panelBox.Location = new System.Drawing.Point(226, 27);
+            this.panelBox.Name = "panelBox";
+            this.panelBox.Size = new System.Drawing.Size(796, 542);
+            this.panelBox.TabIndex = 1;
+            // 
+            // boxTexture
+            // 
+            this.boxTexture.Location = new System.Drawing.Point(3, 3);
+            this.boxTexture.Name = "boxTexture";
+            this.boxTexture.Size = new System.Drawing.Size(100, 50);
+            this.boxTexture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.boxTexture.TabIndex = 0;
+            this.boxTexture.TabStop = false;
+            this.boxTexture.Paint += new System.Windows.Forms.PaintEventHandler(this.boxTexture_Paint);
+            // 
+            // gr1
+            // 
+            this.gr1.Controls.Add(this.typeF);
+            this.gr1.Controls.Add(this.offsetF);
+            this.gr1.Controls.Add(this.len);
+            this.gr1.Controls.Add(this.fName);
+            this.gr1.Controls.Add(this.label4);
+            this.gr1.Controls.Add(this.label3);
+            this.gr1.Controls.Add(this.label2);
+            this.gr1.Controls.Add(this.label1);
+            this.gr1.Location = new System.Drawing.Point(12, 27);
+            this.gr1.Name = "gr1";
+            this.gr1.Size = new System.Drawing.Size(208, 251);
+            this.gr1.TabIndex = 2;
+            this.gr1.TabStop = false;
+            this.gr1.Text = "Properties";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Type:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Offset:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Length:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File name:";
+            // 
+            // gr2
+            // 
+            this.gr2.Location = new System.Drawing.Point(12, 284);
+            this.gr2.Name = "gr2";
+            this.gr2.Size = new System.Drawing.Size(208, 285);
+            this.gr2.TabIndex = 3;
+            this.gr2.TabStop = false;
+            this.gr2.Text = "Data";
+            // 
+            // zoomtrack
+            // 
+            this.zoomtrack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.zoomtrack.Enabled = false;
+            this.zoomtrack.Location = new System.Drawing.Point(682, 0);
+            this.zoomtrack.Maximum = 500;
+            this.zoomtrack.Name = "zoomtrack";
+            this.zoomtrack.Size = new System.Drawing.Size(338, 45);
+            this.zoomtrack.TabIndex = 4;
+            this.zoomtrack.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.zoomtrack.Scroll += new System.EventHandler(this.zoomtrack_Scroll);
+            // 
+            // fName
+            // 
+            this.fName.AutoSize = true;
+            this.fName.Location = new System.Drawing.Point(79, 24);
+            this.fName.Name = "fName";
+            this.fName.Size = new System.Drawing.Size(0, 13);
+            this.fName.TabIndex = 4;
+            // 
+            // len
+            // 
+            this.len.AutoSize = true;
+            this.len.Location = new System.Drawing.Point(79, 43);
+            this.len.Name = "len";
+            this.len.Size = new System.Drawing.Size(0, 13);
+            this.len.TabIndex = 5;
+            // 
+            // offsetF
+            // 
+            this.offsetF.AutoSize = true;
+            this.offsetF.Location = new System.Drawing.Point(79, 61);
+            this.offsetF.Name = "offsetF";
+            this.offsetF.Size = new System.Drawing.Size(0, 13);
+            this.offsetF.TabIndex = 6;
+            // 
+            // typeF
+            // 
+            this.typeF.AutoSize = true;
+            this.typeF.Location = new System.Drawing.Point(79, 77);
+            this.typeF.Name = "typeF";
+            this.typeF.Size = new System.Drawing.Size(0, 13);
+            this.typeF.TabIndex = 7;
+            // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1034, 581);
+            this.Controls.Add(this.gr2);
+            this.Controls.Add(this.gr1);
+            this.Controls.Add(this.panelBox);
+            this.Controls.Add(this.zoomtrack);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
@@ -133,6 +258,12 @@
             this.Text = "Ni no Kuni Wrath of the White Witch Remastered Texture Tool";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.panelBox.ResumeLayout(false);
+            this.panelBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.boxTexture)).EndInit();
+            this.gr1.ResumeLayout(false);
+            this.gr1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.zoomtrack)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -146,11 +277,21 @@
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem getTableCharacterToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem getTextureToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem defaultToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem noAlphaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutMeToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog opf;
+        private System.Windows.Forms.FlowLayoutPanel panelBox;
+        private System.Windows.Forms.PictureBox boxTexture;
+        private System.Windows.Forms.GroupBox gr1;
+        private System.Windows.Forms.GroupBox gr2;
+        private System.Windows.Forms.TrackBar zoomtrack;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label typeF;
+        private System.Windows.Forms.Label offsetF;
+        private System.Windows.Forms.Label len;
+        private System.Windows.Forms.Label fName;
     }
 }
 
--- NinoKuni/main.cs
+++ NinoKuni/main.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Pfim;
 
 namespace NinoKuni
 {
     public partial class main : Form
     {
+
+        private static GCHandle handle;
+        Image imgOriginal;
+
+        Rectangle rect;
+
+        Point LocationXlYt;
+        Point LocationXrYb;
+
         public main()
         {
             InitializeComponent();
@@ -26,16 +38,75 @@ namespace NinoKuni
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            opf.Filter = "All file(*.*)|*.cfg.bin;*.p3igg;*.p3img;*.imgpak|All Font or Table Chars|*.cfg.bin|Header Texture|*.p3img|Texture|*.p3igg|Packet Texture|*.imgpak";
+            opf.Filter = "All file(*.*)|*.p3img;*.imgpak|Header Texture|*.p3img|Packet Texture|*.imgpak";
             if(opf.ShowDialog() == DialogResult.OK)
             {
                 if (!string.IsNullOrEmpty(opf.FileName))
                 {
-                    if(Path.GetExtension(opf.FileName) == ".cfg.bin")
+                    string path = Path.GetDirectoryName(opf.FileName);
+                    string filename = Path.GetFileNameWithoutExtension(opf.FileName);
+                    if(Path.GetExtension(opf.FileName) == ".p3img")
                     {
+                        fName.Text = Path.GetFileName(opf.FileName);
+                        var rdimg = new BinaryReader(new FileStream(opf.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+                        BinaryReader rdigg = null;
+                        BinaryReader rdcfg = null;
+                        bool cfg = false;
+                        bool igg = false;
+                        if (File.Exists(path + "\\" + filename + ".p3igg"))
+                        {
+                            rdigg = new BinaryReader(new FileStream(path + "\\" + filename + ".p3igg", FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+                            igg = true;
+                        }
 
+                        if (File.Exists(path + "\\" + filename + ".cfg.bin"))
+                        {
+                            rdcfg = new BinaryReader(new FileStream(path + "\\" + filename + ".cfg.bin", FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+                            cfg = true;
+                        }
+
+                        if (igg)
+                        {
+                            rdimg.BaseStream.Seek(184, SeekOrigin.Begin);
+                            byte[] head = new byte[] { 0x44, 0x44, 0x53, 0x20, 0x7C, 0x00, 0x00, 0x00, 0x07, 0x10, 0x00, 0x00 };
+                            int width = rdimg.ReadInt16();
+                            int height = rdimg.ReadInt16();
+                            byte[] data_raw = rdigg.ReadBytes((int)rdigg.BaseStream.Length);
+                            len.Text = "0x" + data_raw.Length.ToString("X");
+                            offsetF.Text = "0x0";
+                            if (width * height * 4 == data_raw.Length)
+                            {
+                                typeF.Text = "Texture (RGBA8)";
+                                byte[] done = new byte[head.Length + 12 + argb.Length + data_raw.Length];
+                                Buffer.BlockCopy(head, 0, done, 0, head.Length);
+                                Buffer.BlockCopy(BitConverter.GetBytes(height), 0, done, head.Length, 4);
+                                Buffer.BlockCopy(BitConverter.GetBytes(width), 0, done, head.Length + 4, 4);
+                                Buffer.BlockCopy(BitConverter.GetBytes(data_raw.Length), 0, done, head.Length + 8, 4);
+                                Buffer.BlockCopy(argb, 0, done, head.Length + 12, argb.Length);
+                                Buffer.BlockCopy(data_raw, 0, done, head.Length + 12 + argb.Length, data_raw.Length);
+                                TexDDS(done);
+                            }
+                            else
+                            {
+                                typeF.Text = "Texture (BC7)";
+                                byte[] done = new byte[head.Length + 12 + bc7.Length + data_raw.Length];
+                                Buffer.BlockCopy(head, 0, done, 0, head.Length);
+                                Buffer.BlockCopy(BitConverter.GetBytes(height), 0, done, head.Length, 4);
+                                Buffer.BlockCopy(BitConverter.GetBytes(width), 0, done, head.Length + 4, 4);
+                                Buffer.BlockCopy(BitConverter.GetBytes(data_raw.Length), 0, done, head.Length + 8, 4);
+                                Buffer.BlockCopy(bc7, 0, done, head.Length + 12, bc7.Length);
+                                Buffer.BlockCopy(data_raw, 0, done, head.Length + 12 + bc7.Length, data_raw.Length);
+                                TexDDS(done);
+                            }
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("No p3igg file found!");
+                        }
+                        
                     }
-                    else if(Path.GetExtension(opf.FileName) == ".p3img")
+                    else if(Path.GetExtension(opf.FileName) == ".imgpak")
                     {
 
                     }
@@ -43,8 +114,127 @@ namespace NinoKuni
                     {
 
                     }
+
                 }
             }
+        }
+
+        public void TexDDS(byte[] data)
+        {
+            var image = Pfim.Pfim.FromStream(new MemoryStream(data));
+
+            PixelFormat format;
+            switch (image.Format)
+            {
+                case Pfim.ImageFormat.Rgb24:
+                    format = PixelFormat.Format24bppRgb;
+                    break;
+
+                case Pfim.ImageFormat.Rgba32:
+                    format = PixelFormat.Format32bppArgb;
+                    break;
+
+                case Pfim.ImageFormat.R5g5b5:
+                    format = PixelFormat.Format16bppRgb555;
+                    break;
+
+                case Pfim.ImageFormat.R5g6b5:
+                    format = PixelFormat.Format16bppRgb565;
+                    break;
+
+                case Pfim.ImageFormat.R5g5b5a1:
+                    format = PixelFormat.Format16bppArgb1555;
+                    break;
+
+                case Pfim.ImageFormat.Rgb8:
+                    format = PixelFormat.Format8bppIndexed;
+                    break;
+
+                default:
+                    var msg = $"{image.Format} is not recognized for Bitmap on Windows Forms. " +
+                               "You'd need to write a conversion function to convert the data to known format";
+                    var caption = "Unrecognized format";
+                    MessageBox.Show(msg, caption, MessageBoxButtons.OK);
+                    return;
+            }
+            if (handle.IsAllocated)
+            {
+                handle.Free();
+            }
+
+            handle = GCHandle.Alloc(image.Data, GCHandleType.Pinned);
+            var ptr = Marshal.UnsafeAddrOfPinnedArrayElement(image.Data, 0);
+            var bitmap = new Bitmap(image.Width, image.Height, image.Stride, format, ptr);
+
+            if (format == PixelFormat.Format8bppIndexed)
+            {
+                var palette = bitmap.Palette;
+                for (int i = 0; i < 256; i++)
+                {
+                    palette.Entries[i] = Color.FromArgb((byte)i, (byte)i, (byte)i);
+                }
+                bitmap.Palette = palette;
+            }
+
+            boxTexture.Image = bitmap;
+            imgOriginal = boxTexture.Image;
+        }
+
+        private void zoomtrack_Scroll(object sender, EventArgs e)
+        {
+            if (zoomtrack.Value > 0)
+            {
+                boxTexture.Image = Zoom(imgOriginal, new Size(zoomtrack.Value, zoomtrack.Value));
+            }
+        }
+
+        Image Zoom(Image img, Size size)
+        {
+            Bitmap bmp = new Bitmap(img, img.Width + (img.Width * size.Width / 100), img.Height + (img.Height * size.Height / 100));
+            Graphics g = Graphics.FromImage(bmp);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bmp;
+        }
+
+        public byte[] argb = new byte[]
+        {
+            0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x20, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00,
+            0x00, 0x00, 0x00, 0xFF, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        };
+
+        public byte[] bc7 = new byte[]
+        {
+            0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00, 0x00, 0x44, 0x58, 0x31, 0x30,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x62, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        };
+
+        private void boxTexture_Paint(object sender, PaintEventArgs e)
+        {
+            if(rect != null)
+            {
+                e.Graphics.DrawRectangle(Pens.Red, GetRect());
+            }
+        }
+
+        private Rectangle GetRect()
+        {
+            rect = new Rectangle();
+            rect.X = Math.Min(LocationXlYt.X, LocationXrYb.X);
+            rect.Y = Math.Min(LocationXlYt.Y, LocationXrYb.Y);
+            rect.Width = Math.Abs(LocationXlYt.X - LocationXrYb.X);
+            rect.Height = Math.Abs(LocationXlYt.Y - LocationXrYb.Y);
+            return rect;
         }
     }
 }
